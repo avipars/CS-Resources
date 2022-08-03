c650e093966928f318a72f629b557e45f7d627fb
Avi Parshan <avipars@users.noreply.github.com>
2022-08-03 17:04:04 +0300

    WIP

---
+++ cpp_workshop/Moed_B_2022/q6_hapisga.cpp
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<fstream>
+using namespace std;
+struct Restaurant
+{
+char * name;
+int rating;
+};
+int main()
+{
+ofstream fout("fout.bin");
+Restaurant restaurant;
+restaurant.rating = 10;
+restaurant.name = new char[20];
+strcpy(restaurant.name, "hapisga");;
+fout.write((char*)&restaurant, sizeof(Restaurant));
+fout.close();
+
+//p2
+struct Restaurant
+{
+char* name;
+int rating;
+};
+int main()
+{
+ifstream fin("fout.bin");
+Restaurant rest;
+fin.read((char*)&rest, sizeof(Restaurant));
+cout << rest.name << endl;
+fin.close();
+return 0;
+}
+
+return 0;
+}
